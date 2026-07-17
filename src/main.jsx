eca7b53ae22e149c964c0669c28d8c506ced262d
Corey MacPhee <54730880+cmacphee82@users.noreply.github.com>
2026-07-17 15:09:19 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './water_softener_dual_2026-03-27.jsx'
+import App from '/src/water_softener_dual_2026-03-27.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
